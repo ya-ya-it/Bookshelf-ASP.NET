844451b7b059e73be90186273a6b19a159364566
ya-ya-it <ya-ya-it@yandex.com>
2017-12-10 11:37:09 -0800

    Initial commit

---
+++ Bookshelf_assignment2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bookshelf_assignment2.MvcApplication" Language="C#" %>
---
+++ Bookshelf_assignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bookshelf_assignment2.Startup))]
+namespace Bookshelf_assignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

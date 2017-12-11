ee9018b7c1f0afca15bcd6878fd0a7e7f1b86df3
ya-ya-it <ya-ya-it@yandex.com>
2017-12-10 21:48:50 -0800

    Some magic with ssl

--- Bookshelf_assignment2/App_Start/FilterConfig.cs
+++ Bookshelf_assignment2/App_Start/FilterConfig.cs
@@ -8,6 +8,9 @@ namespace Bookshelf_assignment2
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
+            // force ssl
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }

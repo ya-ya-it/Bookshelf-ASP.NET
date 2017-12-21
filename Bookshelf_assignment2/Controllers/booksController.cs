1978ee91ff0c512b46f6f58249f167c08c2a7d45
ya-ya-it <ya-ya-it@yandex.com>
2017-12-20 19:03:29 -0500

    book controller fixed

--- Bookshelf_assignment2/Controllers/booksController.cs
+++ Bookshelf_assignment2/Controllers/booksController.cs
@@ -53,89 +53,86 @@ namespace Assignment1.Controllers
             return View(book);
         }
 
-        //// GET: books/Create
-        //public ActionResult Create()
-        //{
-        //    ViewBag.author_id = new SelectList(db.authors, "author_id", "last_name");
-        //    return View();
-        //}
+        // GET: books/Create
+        public ActionResult Create()
+        {
+            ViewBag.author_id = new SelectList(db.authors, "author_id", "last_name");
+            return View();
+        }
 
-        //// POST: books/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "book_id,title,author_id,year_published,summary")] book book)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.books.Add(book);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
+        // POST: books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "book_id,title,author_id,year_published,summary")] book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Save(book);
+                return RedirectToAction("Index");
+            }
 
-        //    ViewBag.author_id = new SelectList(db.authors, "author_id", "last_name", book.author_id);
-        //    return View(book);
-        //}
+            ViewBag.author_id = new SelectList(db.authors, "author_id", "last_name", book.author_id);
+            return View(book);
+        }
 
-        //// GET: books/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    book book = db.books.Find(id);
-        //    if (book == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    ViewBag.author_id = new SelectList(db.authors, "author_id", "last_name", book.author_id);
-        //    return View(book);
-        //}
+        // GET: books/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            book book = db.books.SingleOrDefault(b => b.book_id == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.author_id = new SelectList(db.authors, "author_id", "last_name", book.author_id);
+            return View(book);
+        }
 
-        //// POST: books/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "book_id,title,author_id,year_published,summary")] book book)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(book).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    ViewBag.author_id = new SelectList(db.authors, "author_id", "last_name", book.author_id);
-        //    return View(book);
-        //}
+        // POST: books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "book_id,title,author_id,year_published,summary")] book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Save(book);
+                return RedirectToAction("Index");
+            }
+            ViewBag.author_id = new SelectList(db.authors, "author_id", "last_name", book.author_id);
+            return View(book);
+        }
 
-        //// GET: books/Delete/5
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    book book = db.books.Find(id);
-        //    if (book == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(book);
-        //}
+        // GET: books/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            book book = db.books.SingleOrDefault(b => b.book_id == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
 
-        //// POST: books/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    book book = db.books.Find(id);
-        //    db.books.Remove(book);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
+        // POST: books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            book book = db.books.SingleOrDefault(b => b.book_id == id);
+            db.Delete(book);
+            return RedirectToAction("Index");
+        }
 
         //protected override void Dispose(bool disposing)
         //{
